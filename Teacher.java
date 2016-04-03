ec12708b63218e8e71f82f569d56ba37518b9c75
MD ALAM <mdalam7841@gmail.com>
2016-04-03 10:09:04 -0400

    print statment added

--- Teacher.java
+++ Teacher.java
@@ -3,12 +3,13 @@ public class Teacher{
     public static void main(String[] args){
 
 
-        System.out.println("Hello Student!");
+    System.out.println("Hello Student!");
 	System.out.println("Hello Student!");
 	System.out.println("Hello New York!");
-        System.out.println("Hi! Teacher");
+    System.out.println("Hi! Teacher");
  	System.out.println("Hello Someone!");   
 	System.out.println("Bangladesh");
+	System.out.println("MD ALAM");
 
 }
 
