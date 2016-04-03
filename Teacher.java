e20dd37f0cdead127084b34b264f6841aebfe282
Shakil Khan <shakil.khan@kaplan.com>
2016-04-03 10:25:28 -0400

    Code Updated

--- Teacher.java
+++ Teacher.java
@@ -7,17 +7,11 @@ public class Teacher{
 	System.out.println("Hello Student!");
 	System.out.println("Hello New York!");
     System.out.println("Hi! Teacher");
- 	System.out.println("Hello Someone!");   
+ 	System.out.println("Hello Someone!");
 	System.out.println("Bangladesh");
-<<<<<<< HEAD
-	system.out.println("Mohammed Idris");
-=======
-<<<<<<< HEAD
-	
-=======
+	System.out.println("Mohammed Idris");
+
 	System.out.println("MD ALAM");
->>>>>>> ec12708b63218e8e71f82f569d56ba37518b9c75
->>>>>>> e6388947f1262d606fafacf943542e872deaed6d
 
 }
 
