cccdbf376c44f891beb4e6d7dde143e8bf742ea2
MD ALAM <mdalam7841@gmail.com>
2016-04-03 20:07:12 -0400

    alam method

---
+++ AlamGitPractice.java
@@ -0,0 +1,19 @@
+public class AlamGitPractice{
+
+   public static void main(String[]args){
+
+
+   	System.out.println(getSameName(20));
+
+    }
+   
+}
+
+public static int getSameName(int num){
+
+	int num = 0;
+
+    samenum = num;
+
+	return samenum;
+}
