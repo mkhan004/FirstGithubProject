1340a10846db809936ea9748ef391816d31312fd
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-04-05 08:35:01 -0400

    New print statement and compiled.class file is added

--- Teacher.java
+++ Teacher.java
@@ -7,6 +7,8 @@ public class Teacher{
 	System.out.println("Hello Student!");
 	System.out.println("Hello Someone!");
         System.out.println("Hi! Teacher");
+        System.out.println("Hello New York");
+        System.out.println("This is a Computer");
     }
 
 
