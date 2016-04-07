d862ce54b31e56ca6ad7a7cb90b8597ed1342dde
Shermin Aktar <shermintania@gamil.com>
2016-04-07 06:31:41 -0400

    Write a method that take one String input and return last five character as a string.

---
+++ TaniaJaman/TaniaJaman.java
@@ -0,0 +1,19 @@
+public class TaniaJaman {
+    
+	public static void main(String[] args) {
+		String text = "My name is Shamima";
+		System.out.println(getSubString(text));
+	}
+
+	public static String getSubString(String text) {
+		String newText = "";
+		for (int i = text.length() - 5; i < text.length(); i++) {
+		newText = newText + text.charAt(i);
+		}
+		
+
+		return newText;
+	}
+
+}
+
