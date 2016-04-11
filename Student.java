b59ad83f6ca2960b805aef5a91a3e1a6fd46005b
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-04-11 17:11:22 -0400

    New print statement added

--- Student.java
+++ Student.java
@@ -1,7 +1,9 @@
 public class Student {
     public static void main(String[]args){
         System.out.println("Nadia Sultana");
-        System.out.println("Dewan Sultana");   
+        System.out.println("Dewan Sultana");
+        System.out.println("Hello NewYork");
+   
   }
 
 }
