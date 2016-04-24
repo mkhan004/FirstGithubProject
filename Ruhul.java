133a2b5b2207d80cb95410a1a5d3c8d786db57eb
Ruhul Amin <ruhulny10@gmail.com>
2016-04-24 11:58:49 -0400

    I like java statement added

--- Ruhul.java
+++ Ruhul.java
@@ -5,6 +5,7 @@ public static void main(String[] args){
 	System.out.println("Bangladesg");
 	System.out.println("Hello guys ");
 	System.out.println("I like to play cricket");
+	System.out.println("I like my JAVA");
 }
 
 }
