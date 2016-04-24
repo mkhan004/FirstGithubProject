fca01bd636479378e2fff1920a9b4c414072d542
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-04-23 21:51:17 -0400

    Conflict Removed

--- Student.java
+++ Student.java
@@ -9,17 +9,12 @@ public class Student {
         System.out.println("20*20");
         System.out.println("Hasan");
         System.out.println(400+266);
-<<<<<<< HEAD
         System.out.println("20 +20");
-=======
 
         System.out.println("alam change");
 
         System.out.println("my change");
-<<<<<<< HEAD
->>>>>>> c7582ff28cf91ed97e22ec3f63fcf4c6f213afc2
-=======
->>>>>>> c7582ff28cf91ed97e22ec3f63fcf4c6f213afc2
+
   }
 
 }
