5a3608aaa8fb51029bf95d8bb60f8044deda1834
Md Abedin <mdabedinny@gmail.com>
2016-04-26 21:05:38 -0400

    Maximum value findout from an Array solution is added

--- Abedin.java
+++ Abedin.java
@@ -4,4 +4,15 @@ public class Abedin{
 		Systen.out.println(40);
 		Systen.out.println(50);
 	}
+	How to findout maximum value from an Array.
+	public static int maximumValue(int[] array){
+		int maxValue = array[0];
+		for (int i=0; i<array.length; i++) {
+			if(array[i]>maxValue){
+				maximum = array[i];
+			}
+			
+		}
+		return maxValue;
+	}
 }
