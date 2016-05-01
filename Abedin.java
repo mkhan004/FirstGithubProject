e053a8d0cd8fafafc2b32ee88cae7d1be1d6288f
Md Abedin <mdabedinny@gmail.com>
2016-05-01 15:37:17 -0400

    one more print statement is added.

--- Abedin.java
+++ Abedin.java
@@ -1,10 +1,14 @@
 public class Abedin{
+
+	int[] array = {5, 10, 15, 20};
 	public static void main(String[]args){
 		System.out.println(30);
 		Systen.out.println(40);
 		Systen.out.println(50);
+		System.out.println(array);
+		System.out.println(60);
 	}
-	How to findout maximum value from an Array.
+	// How to findout maximum value from an Array.
 	public static int maximumValue(int[] array){
 		int maxValue = array[0];
 		for (int i=0; i<array.length; i++) {
