cc07cc865c1ffcd6cd3ee44365931ef3e0fa044f
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-05-12 23:01:01 -0400

    New print statement change

--- Length.java
+++ Length.java
@@ -1,7 +1,7 @@
 public class Length {
 	public static void main(String[] args) {
 
-    		String text = "Dewan Sultana";
+    		String text = "Dewan Nadia Sultana";
 		System.out.println(text.length());
 		String newText = "";
 
