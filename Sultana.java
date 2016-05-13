0dce7b20af179b225eb09f24531196f8ac6f9e5f
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-05-12 21:31:50 -0400

    Error fixing

--- Sultana.java
+++ Sultana.java
@@ -1,7 +1,12 @@
 Public class Sultana {
     public static void main(String[]args){
-
+        
+        System.out.println("Hello Friend");
         System.out.println("Good Morning");
+        System.out.println("Bangladesh");
+
+
+
    }
 
 
