13f6e07aa8f936f077cf559763147fcafff991c5
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-05-12 21:42:14 -0400

    New unknown change

---
+++ Abedin.java
@@ -0,0 +1,18 @@
+public class Abedin{
+	public static void main(String[]args){
+		System.out.println(30);
+		Systen.out.println(40);
+		Systen.out.println(50);
+	}
+	How to findout maximum value from an Array.
+	public static int maximumValue(int[] array){
+		int maxValue = array[0];
+		for (int i=0; i<array.length; i++) {
+			if(array[i]>maxValue){
+				maximum = array[i];
+			}
+			
+		}
+		return maxValue;
+	}
+}
