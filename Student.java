39e445111fe294a4a7b6733139a052cf49ae34fb
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-05-22 19:36:24 -0400

    Print statement change

--- Student.java
+++ Student.java
@@ -6,7 +6,7 @@ public class Student {
         System.out.println("Good Morning");
         System.out.println("Fatiha");
         System.out.println("Monira");
-        System.out.println("20*20");
+        System.out.println("20+20");
         System.out.println("Hasan");
         System.out.println(400+266);
         System.out.println("20 +20");
