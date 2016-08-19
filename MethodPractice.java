d34004836667b5c0cadce27642530b4dda71d8b9
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-08-19 16:52:21 -0400

    My name change

--- MethodPractice.java
+++ MethodPractice.java
@@ -1,7 +1,7 @@
 public class MethodPractice {
    public static void main(String[]args){
        String[]  nameArray = new String[10];
-       nameArray[0] = "Nadia";
+       nameArray[0] = "Dewan";
        nameArray[3] = "Ruhul";
        nameArray[5] = "Najiat";
        nameArray[6] = "Tasmiyah";
