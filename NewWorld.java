23d4cda3cfae29ffcb43490f0e9c5bc62bc25050
Shermin Aktar <shermintania@gamil.com>
2016-08-19 23:45:31 -0400

    Some statements are added

--- NewWorld.java
+++ NewWorld.java
@@ -1,7 +1,7 @@
 public class NewWorld{
 
     public static void main(String[] args) {
-
+   System.out.println("This is a computer");
    System.out.println(getLowerCase("This is a String"));
 	}
 	public static String getLowerCase(String text){
