d6ba7c139ce9f7a8d9477a5da96c138f4090f386
RUMA <ruma@RUMAs-MacBook-Pro.local>
2016-08-27 18:59:15 -0400

    A new class added.

---
+++ AlphabetPractice.java
@@ -0,0 +1,14 @@
+package ultimate.practice;
+
+public class AlphabetPractice {
+	public static void main(String args[]) {
+		char ch;
+		for (ch = 'a'; ch <= 'z'; ch++)
+			System.out.println(ch);
+	}
+}
+// public static String getAlphabet(char aCharacter) {
+// String alphabet = "";
+// for (aCharacter = 'a'; aCharacter <= 'z'; aCharacter++)
+// System.out.print(aCharacter);
+// return alphabet;
