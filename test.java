202f6a69e1001f81b458c95c992272f06f2d43fd
Md Abedin <mdabedinny@gmail.com>
2016-12-08 16:52:24 -0500

    test file added

--- test.java
+++ test.java
@@ -2,9 +2,30 @@
 
 public class test{
 	
-	public static void main(String[]args){
+	public static void main(String[] args) {
+		//getDateOfYear();
+		getAge();
+	
 
-        System.out.println("Hello Bangladesg");
-
+	}
+	
+	public static int getDateOfYear(){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your age");
+		int age= input.nextInt();
+		int year= 2016 - age;
+		System.out.println("you are probabily bron in" + year);
+		return year;
+	}
+	
+	public static int getAge(){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What Year you bron");
+		int year= input.nextInt();
+		int age= 2016 - year;
+		System.out.println("your age is probabily " + age);
+		return year;
 	}
 }
